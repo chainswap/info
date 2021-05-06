8193a6538c2cd7a1337e2b0e4b086f2e222870f1
marvincwhuang <cwhuang0523@gmail.com>
2021-05-07 00:17:07 +0800

    implement Row and Row Between

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { styled } from '@material-ui/styles'
-import DropDown from '../../assets/images/dummy_logo.png'
 
 const LabelRow = styled('div')({
   fontWeight: 400,
--- src/components/CurrencySelectPanel/index.tsx
+++ src/components/CurrencySelectPanel/index.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
-import { MenuItem } from '@material-ui/core'
+import { InputLabel } from '@material-ui/core'
 import { styled } from '@material-ui/styles'
 import Select from '../Select/Select'
+import SwitchArrow from '../../assets/images/switch_arrow.svg'
+import { RowBetween } from '../../components/Row'
 
-const CurrencySelectFrom = styled('div')({
+const CurrencySelect = styled('div')({
   width: 176,
   height: 46,
-  backgroundColor: '1f1f1f',
   boxSizing: 'border-box',
   borderRadius: 14,
-  display: 'flex',
   alignItems: 'center',
+})
+
+const CurrencySelectLabel = styled('div')({
+  color: '#FFFFFF',
+  opacity: 0.6,
+  fontSize: 12,
+  fontWeight: 400,
+  fontFamily: 'Roboto',
+  lineHeight: '17.48px',
 })
 
 export default function CurrencySelectPanel() {
   return (
-    <CurrencySelectFrom>
-      <Select defaultValue="ETH" disabled>
-        <option value="ETH">ETH</option>
-      </Select>
-      <Select defaultValue="ETH">
-        <option value="ETH">ETH</option>
-      </Select>
-    </CurrencySelectFrom>
+    <>
+      <RowBetween>
+        <CurrencySelect>
+          <CurrencySelectLabel>From</CurrencySelectLabel>
+          <Select defaultValue="ETH" disabled>
+            <option value="ETH">ETH</option>
+          </Select>
+        </CurrencySelect>
+        <img src={SwitchArrow} alt={'switch_arrow_icon'} />
+        <CurrencySelect>
+          <CurrencySelectLabel>To</CurrencySelectLabel>
+          <Select defaultValue="ETH">
+            <option value="ETH">ETH</option>
+          </Select>
+        </CurrencySelect>
+      </RowBetween>
+    </>
   )
 }

060a2ecb281678895aae4940e725983d8bb35597
marvincwhuang <cwhuang0523@gmail.com>
2021-05-07 01:40:19 +0800

    temp BodyWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Demo from './pages/Demo'
 import Swap from './pages/Swap'
+import { styled } from '@material-ui/core'
+
+const BodyWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  paddingTop: 200,
+  alignItems: 'center',
+})
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact strict path="/demo" component={Demo} />
-        <Route exact strict path="/swap" component={Swap} />
-      </Switch>
-    </BrowserRouter>
+    <BodyWrapper>
+      <BrowserRouter>
+        <Switch>
+          <Route exact strict path="/demo" component={Demo} />
+          <Route exact strict path="/swap" component={Swap} />
+        </Switch>
+      </BrowserRouter>
+    </BodyWrapper>
   )
 }

83202636d938da650bd9ffd3e5b2b0ed14ae68f6
marvincwhuang <cwhuang0523@gmail.com>
2021-05-09 19:16:23 +0800

    add seperator

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,4 +1,18 @@
-import { styled } from '@material-ui/styles'
+import { styled, makeStyles } from '@material-ui/styles'
+interface Props {
+  gap?: string
+  justify?: 'stretch' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'space-between'
+  children?: React.ReactNode
+}
+
+const useStyles = makeStyles({
+  autoColumn: {
+    display: 'grid',
+    gridAutoRows: 'auto',
+    gridRowGap: (props: Props) => props.gap,
+    justifyItems: (props: Props) => props.justify,
+  },
+})
 
 const Column = styled('div')({
   display: 'flex',
@@ -10,5 +24,11 @@ export const ColumnCenter = styled(Column)({
   width: '100%',
   alignItems: 'center',
 })
+
+export function AutoColumn(props: Props) {
+  const classes = useStyles(props)
+
+  return <div className={classes.autoColumn}>{props.children}</div>
+}
 
 export default Column
--- src/components/SearchModal/CurrencySearch.tsx
+++ src/components/SearchModal/CurrencySearch.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { RowBetween } from '../Row/index'
 import { styled } from '@material-ui/styles'
 import { Input } from '@material-ui/core'
+import { AutoColumn } from '../Column/index'
 
 const SearchInput = styled(Input)({
   position: 'relative',
@@ -15,10 +16,19 @@ const SearchInput = styled(Input)({
   height: 48,
   borderRadius: 14,
   boxSizing: 'border-box',
-  border: '1px solid hsla(0,0%,100%,.4)',
+  border: '1px solid rgba(255, 255, 255, 0.4)',
   background: 'hsla(0,0%,100%,.08)',
 })
 
+// const PaddedColumn = styled(AutoColumn)({
+//   padding: 20,
+// })
+
+const Seperator = styled('div')({
+  width: '100%',
+  height: 1,
+  backgroundColor: 'hsla(0,0%,100%,.12)',
+})
 interface Props {
   isOpen: boolean
   onDismiss: () => void
@@ -34,7 +44,10 @@ interface Props {
 export default function CurrencySearch(props: Props) {
   return (
     <>
-      <SearchInput disableUnderline />
+      <AutoColumn gap="16px">
+        <SearchInput disableUnderline placeholder={'Search by name or paste address'} />
+        <Seperator />
+      </AutoColumn>
     </>
   )
 }

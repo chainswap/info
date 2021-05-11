690f7fce9f080ccd3d3db98b0941485e34cd7850
marvincwhuang <cwhuang0523@gmail.com>
2021-05-11 22:20:17 +0800

    basic Stepper

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
-import { Stepper, Step, StepLabel } from '@material-ui/core'
+import MuiStepper from '@material-ui/core/Stepper'
+import MuiStep from '@material-ui/core/Step'
+import MuiStepIcon from '@material-ui/core/StepIcon'
+import { withStyles } from '@material-ui/core'
+
+const Stepper = withStyles({
+  root: {
+    background: 'transparent',
+  },
+  horizontal: {
+    padding: 0,
+  },
+  text: {
+    opacity: 1,
+  },
+})(MuiStepper)
+
+const Step = withStyles({
+  root: {},
+  horizontal: {
+    padding: 0,
+  },
+})(MuiStep)
+
+const StepIcon = withStyles({
+  root: {
+    border: '1px solid white',
+    borderRadius: '50%',
+    '&$active': {
+      color: 'transparent',
+    },
+  },
+  active: {},
+  horizontal: {
+    padding: 0,
+  },
+})(MuiStepIcon)
 
 export default function _Stepper() {
   const steps = [1, 2]
@@ -7,7 +43,7 @@ export default function _Stepper() {
     <Stepper>
       {steps.map((label) => (
         <Step key={label}>
-          <StepLabel>{label}</StepLabel>
+          <StepIcon icon={label}></StepIcon>
         </Step>
       ))}
     </Stepper>

c385317eee34d15c8c769df10ca616ad5dc3c043
marvincwhuang <cwhuang0523@gmail.com>
2021-05-14 00:25:57 +0800

    basic structure

--- src/components/swap/ConfirmDepositModal.tsx
+++ src/components/swap/ConfirmDepositModal.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
 import Modal from '../Modal/Modal'
+import Button from '../Button/Button'
 
 interface Props {
   isOpen: boolean
@@ -8,7 +10,25 @@ interface Props {
   label?: string
 }
 
+const SwapCurrency = () => {
+  return <Box>ETH - BSC</Box>
+}
+
 export default function ConfirmDepositModal(props: Props) {
   const { isOpen, onDismiss } = props
-  return <Modal isOpen={isOpen} onDismiss={onDismiss} label={'Confirm Deposit'} />
+  return (
+    <>
+      <Modal isOpen={isOpen} onDismiss={onDismiss} label={'Confirm Deposit'}>
+        <Box>400 MATTER</Box>
+        <Box>
+          <SwapCurrency />
+        </Box>
+        <Box>
+          <Box>Destination Chain Address</Box>
+          <Box>0x72ef586A2c515...D43Df77123</Box>
+        </Box>
+        <Button size="large">Confirm</Button>
+      </Modal>
+    </>
+  )
 }

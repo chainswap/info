d2d24f66e4c7ecccfea960a9cacc97b0157528df
marvincwhuang <cwhuang0523@gmail.com>
2021-05-17 22:14:02 +0800

    refactor

--- src/components/swap/ConfirmWithdrawModal.tsx
+++ src/components/swap/ConfirmWithdrawModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box } from '@material-ui/core'
 import { styled } from '@material-ui/styles'
 import Modal from '../Modal/Modal'
@@ -8,6 +8,11 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import Currency from '../../models/currency'
 import { TYPE } from '../../theme/index'
 import { Text } from 'rebass'
+
+enum Mode {
+  INSTRUCTION,
+  CONFIRM,
+}
 
 interface Props {
   isOpen: boolean
@@ -29,6 +34,21 @@ const Label = styled('label')({
   marginRight: '12px',
 })
 
+const FadedBox = styled(Box)({
+  opacity: 0.2,
+})
+
+const Instruction = ({ to }: { to: Currency; faded?: boolean }) => {
+  return (
+    <Box color="#FFFFFF" padding="40px 32px 0" fontSize="18px">
+      <TYPE.smallheader textAlign="center">
+        1. Please switch <strong>your wallet network</strong> to {to.symbol} to complete token swap. 2. Also please{' '}
+        <strong>switch to your wallet</strong> with the destination address
+      </TYPE.smallheader>
+    </Box>
+  )
+}
+
 const SwapCurrency = ({ from, to }: { from: Currency; to: Currency }) => {
   // const {fromLogo, from, toLogo, to} = props
   return (
@@ -36,7 +56,7 @@ const SwapCurrency = ({ from, to }: { from: Currency; to: Currency }) => {
       bgcolor="rgba(255, 255, 255, 0.08)"
       borderRadius="10px"
       height="48px"
-      margin="0 32px"
+      margin="28px 32px 0"
       display="flex"
       alignItems="center"
       justifyContent="space-between"
@@ -57,6 +77,28 @@ const SwapCurrency = ({ from, to }: { from: Currency; to: Currency }) => {
   )
 }
 
+const ChainAddress = ({ walletLogo, address }: { walletLogo: string; address: string }) => {
+  const trimmedAddress = (address: string) => {
+    const limit = 25
+
+    if (address.length > limit) {
+      return address.substring(0, 20) + '...' + address.substr(-5)
+    }
+  }
+
+  return (
+    <Box color="#FFFFFF" display="flex" justifyContent="space-between" margin="16px 32px 0 32px">
+      <Label>Destination Chain Address:</Label>
+      <Box display="flex" alignItems="center">
+        <img src={walletLogo} alt={'wallet logo'} />
+        <Box marginLeft="8px" fontSize="12px">
+          {trimmedAddress(address)}
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
 const Seperator = styled('div')({
   width: '100%',
   height: 1,
@@ -67,49 +109,48 @@ const Seperator = styled('div')({
 
 export default function ConfirmWithdrawModal(props: Props) {
   const { isOpen, onDismiss, from, to, walletLogo, address, value, selectedCurrency, onConfirm } = props
-
-  const trimmedAddress = (address: string) => {
-    const limit = 25
-
-    if (address.length > limit) {
-      return address.substring(0, 20) + '...' + address.substr(-5)
-    }
-  }
+  const [mode, setMode] = useState(Mode.INSTRUCTION)
 
   return (
     <>
       <Modal isOpen={isOpen} onDismiss={onDismiss}>
-        <Box color="#FFFFFF" padding="40px 32px 28px" fontSize="18px">
-          <TYPE.smallheader textAlign="center">
-            1. Please switch <strong>your wallet network</strong> to BSC to complete token swap. 2. Also please{' '}
-            <strong>switch to your wallet</strong> with the destination address
-          </TYPE.smallheader>
-        </Box>
-        <Box>
-          <SwapCurrency from={from} to={to} />
-        </Box>
-        <Box color="#FFFFFF" display="flex" justifyContent="space-between" margin="16px 32px 0 32px">
-          <Label>Destination Chain Address:</Label>
-          <Box display="flex" alignItems="center">
-            <img src={walletLogo} alt={'wallet logo'} />
-            <Box marginLeft="8px" fontSize="12px">
-              {trimmedAddress(address)}
+        {mode === Mode.INSTRUCTION ? (
+          <>
+            <Instruction to={to} />
+            <SwapCurrency from={from} to={to} />
+            <ChainAddress walletLogo={walletLogo} address={address} />
+            <Seperator />
+            <FadedBox>
+              <Box marginBottom="12px">
+                <TYPE.subheader textAlign={'center'}>3. Confirm Withdraw</TYPE.subheader>
+              </Box>
+              <Box>
+                <Text fontWeight={'500'} fontSize={'28px'} textAlign={'center'}>
+                  {value} {selectedCurrency.symbol}
+                </Text>
+              </Box>
+            </FadedBox>
+          </>
+        ) : (
+          <>
+            <FadedBox>
+              <Instruction to={to} />
+            </FadedBox>
+            <Seperator />
+            <Box marginBottom="12px">
+              <TYPE.subheader textAlign={'center'}>3. Confirm Withdraw</TYPE.subheader>
             </Box>
-          </Box>
-        </Box>
-        <Seperator />
-        <Box marginBottom="12px">
-          <TYPE.subheader textAlign={'center'} opacity={0.2}>
-            3. Confirm Withdraw
-          </TYPE.subheader>
-        </Box>
-        <Box>
-          <Text fontWeight={'500'} fontSize={'28px'} textAlign={'center'} opacity={0.2}>
-            {value} {selectedCurrency.symbol}
-          </Text>
-        </Box>
+            <Box>
+              <Text fontWeight={'500'} fontSize={'28px'} textAlign={'center'}>
+                {value} {selectedCurrency.symbol}
+              </Text>
+            </Box>
+            <SwapCurrency from={from} to={to} />
+            <ChainAddress walletLogo={walletLogo} address={address} />
+          </>
+        )}
         <Box margin="28px 32px 29px">
-          <Button size="large" onClick={onConfirm} disabled>
+          <Button size="large" onClick={onConfirm} disabled={mode === Mode.INSTRUCTION}>
             Confirm
           </Button>
         </Box>

bdfcec91a34620fd170e3440b3f80d6cf44f2664
marvincwhuang <cwhuang0523@gmail.com>
2021-06-07 22:52:16 +0800

    seperate wallet and account modal

--- src/components/AccountModal/AccountModal.tsx
+++ src/components/AccountModal/AccountModal.tsx
@@ -10,6 +10,7 @@ import OutlineButton from '../Button/OutlineButton'
 import Button from '../Button/Button'
 import Transaction from './Transaction'
 import { ModalContext } from '../../context/ModalContext'
+import Modal from '../Modal/Modal'
 
 interface Props {
   pendingTransactions: string[]
@@ -48,11 +49,7 @@ export default function AccountDetails(props: Props) {
   const { openOptions, pendingTransactions, confirmedTransactions } = props
   const name = 'MetaMask'
   const account = '0x72ef586A2c515B605A873ad9a8FBdFD43Df77123'
-  const { hideModal } = useContext(ModalContext)
-
-  function getStatusIcon() {
-    return <Image src={StatusIcon} alt={'status icon'} style={{ width: 28, height: 28 }} />
-  }
+  const { isOpen, hideModal } = useContext(ModalContext)
 
   function renderTransactions(transactions: string[]) {
     return (
@@ -65,10 +62,10 @@ export default function AccountDetails(props: Props) {
   }
 
   return (
-    <>
+    <Modal isOpen={isOpen} onDismiss={hideModal} showIcon={false}>
       <Header>Connected with {name}</Header>
       <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} mx={'141px'}>
-        {getStatusIcon()}
+        <Image src={StatusIcon} alt={'status icon'} style={{ width: 28, height: 28 }} />
         <Text fontSize={24} fontWeight={400}>
           {account && shortenAddress(account)}
         </Text>
@@ -95,6 +92,6 @@ export default function AccountDetails(props: Props) {
           {renderTransactions(confirmedTransactions)}
         </TransactionListItemsWrapper>
       </TransactionListWrapper>
-    </>
+    </Modal>
   )
 }
--- src/components/WalletModal/WalletModal.tsx
+++ src/components/WalletModal/WalletModal.tsx
@@ -4,7 +4,7 @@ import { Box } from '@material-ui/core'
 import { styled } from '@material-ui/styles'
 import { SUPPORTED_WALLETS } from '../../constants'
 import Option from './Option'
-import AccountDetails from '../AccountDetails/AccountDetails'
+import AccountDetails from '../AccountModal/AccountModal'
 import { ModalContext } from '../../context/ModalContext'
 
 import { useSetUser, useUserLogined } from '../../state/user/hooks'
@@ -22,23 +22,18 @@ const Header = styled(Box)({
 
 export default function WalletModal() {
   const [walletView, setWalletView] = useState(WALLET_VIEWS.OPTIONS)
-  const { isOpen, hideModal } = useContext(ModalContext)
+  const { isOpen, showModal, hideModal } = useContext(ModalContext)
   const userLogined = useUserLogined()
   const setUser = useSetUser()
 
-  const onClickOption = () => {
-    setUser({ address: '1234' })
-    setWalletView(WALLET_VIEWS.ACCOUNT)
-  }
-
-  function getOptions() {
+  const getOptions = () => {
     return Object.keys(SUPPORTED_WALLETS).map((key) => {
       const option = SUPPORTED_WALLETS[key]
 
       return (
         <Option
           id={`connect-${key}`}
-          onClick={onClickOption}
+          onClick={showAccountModal}
           key={key}
           color={option.color}
           link={option.href}
@@ -50,34 +45,26 @@ export default function WalletModal() {
     })
   }
 
-  function getModalContent() {
-    if (userLogined && walletView === WALLET_VIEWS.ACCOUNT) {
-      return (
-        <AccountDetails
-          openOptions={() => setWalletView(WALLET_VIEWS.OPTIONS)}
-          ENSName={'0xe60b...e6d3'}
-          pendingTransactions={['Swap 1.0ETH for 0.000000001 BSC']}
-          confirmedTransactions={['Swap 1.0ETH for 0.000000001 BSC', 'Swap 1.0ETH for 0.000000001 BSC']}
-        />
-      )
-    }
-
-    return (
-      <>
-        <Box width={480} padding="32px" display="flex" flexDirection="column" alignItems="center">
-          <Header>Connect to a wallet</Header>
-
-          <Box display="grid" gridGap="12px">
-            {getOptions()}
-          </Box>
-        </Box>
-      </>
+  const showAccountModal = () => {
+    showModal(
+      <AccountDetails
+        openOptions={() => setWalletView(WALLET_VIEWS.OPTIONS)}
+        ENSName={'0xe60b...e6d3'}
+        pendingTransactions={['Swap 1.0ETH for 0.000000001 BSC']}
+        confirmedTransactions={['Swap 1.0ETH for 0.000000001 BSC', 'Swap 1.0ETH for 0.000000001 BSC']}
+      />
     )
   }
 
   return (
     <Modal isOpen={isOpen} onDismiss={hideModal} showIcon={walletView === WALLET_VIEWS.OPTIONS}>
-      {getModalContent()}
+      <Box width={480} padding="32px" display="flex" flexDirection="column" alignItems="center">
+        <Header>Connect to a wallet</Header>
+
+        <Box display="grid" gridGap="12px">
+          {getOptions()}
+        </Box>
+      </Box>
     </Modal>
   )
 }

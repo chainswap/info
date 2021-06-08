03de75718da5a61bfa791850bf8ad166ab09db98
iv172 <iv172@hotmail.com>
2021-06-08 20:10:14 +0800

    add TYPE

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
-import { createMuiTheme, Theme } from '@material-ui/core'
+import { createMuiTheme, Theme, styled, useTheme } from '@material-ui/core'
+import { Text, TextProps } from 'rebass'
+
+interface TextColor {
+  '1': string
+  '2': string
+  '3': string
+  '4': string
+  '5': string
+}
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface ThemeOptions {
+    textColor?: TextColor
+  }
+  interface Theme {
+    textColor?: TextColor
+  }
+}
 
 const theme: Theme = createMuiTheme({
   palette: {
@@ -18,6 +34,42 @@ const theme: Theme = createMuiTheme({
       disabledOpacity: 0.6,
     },
   },
+  textColor: {
+    '1': '#FFFFFF',
+    '2': '#CCCCCC',
+    '3': '#999999',
+    '4': '#727272',
+    '5': '#333333',
+  },
 })
 
 export default theme
+
+const TextWrapper = ({ textColor, ...props }: TextProps & { textColor?: keyof TextColor }) => {
+  const theme = useTheme()
+  return <Text {...props} color={textColor ? theme?.textColor?.[textColor] : undefined} />
+}
+
+export const TYPE = {
+  body(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={16} textColor={'1'} {...props} />
+  },
+  largeHeader(props: TextProps) {
+    return <TextWrapper fontWeight={600} fontSize={24} {...props} fontFamily="Futura PT" />
+  },
+  mediumHeader(props: TextProps) {
+    return <TextWrapper fontWeight={500} fontSize={22} {...props} fontFamily="Futura PT" />
+  },
+  smallHeader(props: TextProps) {
+    return <TextWrapper fontWeight={500} fontSize={16} {...props} fontFamily="Futura PT" />
+  },
+  small(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={12} {...props} />
+  },
+  medium(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={14} {...props} />
+  },
+  smallGray(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={12} textColor={'3'} {...props} />
+  },
+}

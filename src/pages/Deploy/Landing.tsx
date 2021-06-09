78d7fda9eb37b085e71a47961565a87784163ca9
marvincwhuang <cwhuang0523@gmail.com>
2021-06-10 00:19:52 +0800

    use Text

--- src/pages/Deploy/Landing.tsx
+++ src/pages/Deploy/Landing.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import { Box } from '@material-ui/core'
-import { styled } from '@material-ui/styles'
 import OutlineButton from '../../components/Button/OutlineButton'
 import { Text } from 'rebass'
+import theme from '../../theme/index'
 
 interface Props {
   onClickExistingToken: () => void
   onClickNewToken: () => void
 }
-
-const Hint = styled('div')({
-  fontSize: 12,
-  height: 18,
-  fontWeight: 400,
-  color: 'white',
-  opacity: 0.8,
-  margin: 0,
-  marginTop: 3,
-})
 
 export default function Landing(props: Props) {
   const { onClickExistingToken, onClickNewToken } = props
@@ -31,13 +21,17 @@ export default function Landing(props: Props) {
         <OutlineButton onClick={onClickExistingToken} height={72} primary>
           <Box display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
             Existing Token
-            <Hint>You already deployed a token on Ethereum or EMV supportive chians</Hint>
+            <Text fontWeight={400} fontSize={12} opacity={0.8} color={theme.textColor.text1}>
+              You already deployed a token on Ethereum or EMV supportive chians
+            </Text>
           </Box>
         </OutlineButton>
         <OutlineButton onClick={onClickNewToken} height={72} primary>
           <Box display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'}>
             New Token
-            <Hint>You already deployed a token on Ethereum or EMV supportive chians</Hint>
+            <Text fontWeight={400} fontSize={12} opacity={0.8} color={theme.textColor.text1}>
+              You already deployed a token on Ethereum or EMV supportive chians
+            </Text>
           </Box>
         </OutlineButton>
       </Box>

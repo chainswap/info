93081bd4d39fdd5486232536b2ba08ed99552568
marvincwhuang <cwhuang0523@gmail.com>
2021-06-22 00:13:12 +0800

    complete mapping form

--- src/pages/Deploy/DeployMappingForm.tsx
+++ src/pages/Deploy/DeployMappingForm.tsx
@@ -1,6 +1,8 @@
 import React, { SyntheticEvent } from 'react'
 import { Box, styled } from '@material-ui/core'
 import Input from 'components/Input/Input'
+import { Text } from 'rebass'
+import theme from 'theme'
 
 interface Props {
   tokenAddress: string
@@ -14,25 +16,30 @@ interface Props {
 export default function DeployMappingForm(props: Props) {
   const { tokenAddress, mappableAddress, mainChainId, onTokenAddress, onMappableAddress, onMainChainId } = props
   return (
-    <Box display="grid" gridGap="24px">
-      <Input
-        label="Token Contract Address"
-        placeholder="Enter the token contract address"
-        value={tokenAddress}
-        onChange={onTokenAddress}
-      />
-      <Input
-        label="Mappable Contract Address"
-        placeholder="Enter the mappable contract address"
-        value={mappableAddress}
-        onChange={onMappableAddress}
-      />
-      <Input
-        label="Token Contract Address"
-        placeholder="Enter the mainchain ID"
-        value={mainChainId}
-        onChange={onMainChainId}
-      />
-    </Box>
+    <>
+      <Text color={theme.textColor.text2} fontSize={16} mb="20px">
+        Please fill the Mainchain Info
+      </Text>
+      <Box display="grid" gridGap="24px">
+        <Input
+          label="Token Contract Address"
+          placeholder="Enter the token contract address"
+          value={tokenAddress}
+          onChange={onTokenAddress}
+        />
+        <Input
+          label="Mappable Contract Address"
+          placeholder="Enter the mappable contract address"
+          value={mappableAddress}
+          onChange={onMappableAddress}
+        />
+        <Input
+          label="Token Contract Address"
+          placeholder="Enter the mainchain ID"
+          value={mainChainId}
+          onChange={onMainChainId}
+        />
+      </Box>
+    </>
   )
 }

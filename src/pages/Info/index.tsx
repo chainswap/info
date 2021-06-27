4ef4d6b27a7d92f9c436f7f3ba33c95467a896a1
marvincwhuang <cwhuang0523@gmail.com>
2021-06-27 22:54:40 +0800

    updae stats spacing

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -26,7 +26,7 @@ export default function Info() {
   return (
     <AppBody title="ChainSwap Stats" width={640}>
       <Wrapper>
-        <Grid container spacing={2}>
+        <Grid container spacing={1}>
           <Grid item xs={12}>
             <MainCard>
               <TYPE.extraSmallGray>TOTAL SWAAPED ASSETS VALUE</TYPE.extraSmallGray>
